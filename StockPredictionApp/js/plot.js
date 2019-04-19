f096224e66f755466f4c70ad3a5ff317448bd445
VitalPointAI <a.luhning@vitalpoint.ai>
2019-04-19 12:39:24 -0400

    trying to shift predictions to right

--- StockPredictionApp/js/plot.js
+++ StockPredictionApp/js/plot.js
@@ -6,7 +6,13 @@ const plotData = function (data1, data2, label = null) {
     //limit chart data to last X number of data points
     var newdata1 = data1.slice(-50);
     var newdata2 = data2.slice(-50);
+    //shift predictions to right to properly match actuals
+    newdata2 = newdata2.unshift(0);
     var N = label ? label : [...Array(Math.max(newdata1.length, newdata2.length)).keys()];
+    var tomorrow = new Date(N[-1]);
+    var predictionDate = tomorrow.setDate(tomorrow+1);
+    console.log(predictionDate);
+    N.push(predictionDate);
     var newN = N.slice(-50);
 
     var config = {
@@ -53,7 +59,7 @@ const plotData = function (data1, data2, label = null) {
                     display: true,
                     scaleLabel: {
                         display: true,
-                        labelString: 'Stock Value'
+                        labelString: 'Crypto Value'
                     }
                 }]
             }
